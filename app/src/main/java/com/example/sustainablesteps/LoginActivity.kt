c853eab531c81053df5970a8b5b6706e06ccfec5
Ananya54321 <ananyapappula2004@gmail.com>
2023-11-29 10:33:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sustainablesteps/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sustainablesteps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sustainablesteps.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private val binding : ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.dontHaveAnAccount.setOnClickListener{
+            val intent = Intent(this, SignUpActiviity::class.java)
+            startActivity(intent)
+        }
+        binding.logInButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
